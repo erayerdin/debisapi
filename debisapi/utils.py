e2eb53abb700d5fe1256de90aad1adbf3a5f0c68
Eray Erdin <eraygezer.94@gmail.com>
2019-06-12 23:23:46 +0300

    added get_token and get_payloads utils

---
+++ debisapi/utils.py
@@ -0,0 +1,20 @@
+import jwt
+from django.conf import settings
+
+
+def get_token(request):
+    author_header = request.META.get("Authorization", None)
+
+    if author_header is None:
+        return None
+
+    return author_header[6:]
+
+
+def get_payload(request):
+    token = get_token(request)
+
+    if token is None:
+        return None
+
+    return jwt.decode(token, settings.SECRET_KEY, algorithms="HS256")
